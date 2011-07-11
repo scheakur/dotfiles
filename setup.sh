c1a0b7b432107a3e9551e3c0678022b87eb34151
takuro <takuro@ubuntu.(none)>
2011-07-11 16:26:23 +0900

    added tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 path=$(pwd)
-target_list=(vimrc vim gvimrc)
+target_list=(vimrc vim gvimrc tmux.conf)
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
