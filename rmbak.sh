3415e568bb7aa702beb7e04034a7dd9e5b806c27
takuro <takuro@ubuntu.(none)>
2011-07-11 16:28:21 +0900

    added tmux.conf and script file

---
+++ rmbak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+target_list=(vimrc vim gvimrc tmux.conf)
+for (( i = 0; i < ${#target_list[@]}; i++ ))
+do
+    target=${target_list[i]}
+    if [ -e ~/.$target.orig.back ]; then
+        rm ~/.$target.orig.back
+    fi
+done
