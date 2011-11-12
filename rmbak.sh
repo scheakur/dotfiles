4fd1db37a8c15b6dab1ff75ad3dd1ea0c4200e3e
scheakur <scheakur@gmail.com>
2011-11-12 11:33:41 +0900

    Update setup scripts to work well in Mac.

--- rmbak.sh
+++ rmbak.sh
@@ -4,7 +4,7 @@ target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore)
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
-    if [ -e ~/.$target.orig.back ]; then
+    if [ -e ~/.$target.orig.back -o -L ~/.$target.orig.back ]; then
         rm ~/.$target.orig.back
     fi
 done
--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore)
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
-    if [ -e ~/.$target ]; then
+    echo $target
+    if [ -e ~/.$target -o -L ~/.$target ]; then
+	echo $target
         mv ~/.$target ~/.$target.orig.back
     fi
     ln -s $path/$target ~/.$target
