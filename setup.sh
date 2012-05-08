da29dcb3788f6871edc4b88b43968947d3428d27
scheakur <scheakur@gmail.com>
2012-05-09 01:02:22 +0900

    Do git clone for vim config in setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# Make symbolic links.
 path=$(pwd)
-target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore)
+target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore zshrc)
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
-    echo $target
     if [ -e ~/.$target -o -L ~/.$target ]; then
-	echo $target
+	if [ -e ~/.$target.orig.back -o -L ~/.$target.orig.back ]; then
+	    rm -rf ~/.$target.orig.back 
+	fi
         mv ~/.$target ~/.$target.orig.back
     fi
     ln -s $path/$target ~/.$target
 done
+
+
+# Clone vundle.vim for vim.
+if [ -e ~/.vim/bundle/vundle ]; then
+    rm -rf ~/.vim/bundle/vundle
+fi
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
