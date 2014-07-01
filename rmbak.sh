87c38c81166b0ff7519aba4df32bfd28d4062a85
scheakur <scheakur@gmail.com>
2014-07-01 10:05:09 +0900

    Update target list to correspond to setup.sh

--- rmbak.sh
+++ rmbak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore)
+target_list=(vimrc vim gvimrc tmux.conf vrapperrc gitconfig gitignore profile zshrc zshenv config/peco)
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
