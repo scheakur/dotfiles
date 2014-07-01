93cbbb2547be8312b3be87ed82ebde4e4b2df203
scheakur <scheakur@gmail.com>
2014-07-01 12:18:10 +0900

    Add snippets for sshfs

--- config/peco/mysnippets.sh
+++ config/peco/mysnippets.sh
@@ -8,3 +8,7 @@ cd ~/app/addon-sdk-1.16/; source bin/activate; cd - #addon
 svn info | grep -E '^Revision: ' | sed 's/Revision: /r/' | xsel -ib #copyrevision
 git svn info | grep -E '^Revision: ' | sed 's/Revision: /r/' | xsel -ib #copyrevision
 
+## sshfs
+sshfs user@hostname: ~/mnt
+fusermount -u ~/mnt
+
