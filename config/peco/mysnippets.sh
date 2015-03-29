6029bd2774c708516ef69a7ec2de90212ea3ea1f
scheakur <scheakur@gmail.com>
2015-03-29 10:36:52 +0900

    Update version of cfx

--- config/peco/mysnippets.sh
+++ config/peco/mysnippets.sh
@@ -2,7 +2,7 @@
 chromium-browser --temp-profile --allow-file-access-from-files --incognito ~/Dropbox/etc/1Password.agilekeychain/1Password.html #1password
 
 # Activate Firefox Add-on SDK
-cd ~/app/addon-sdk-1.16/; source bin/activate; cd - #addon
+cd ~/app/addon-sdk-1.17/; source bin/activate; cd - #addon
 
 # Copy revision
 svn info | grep -E '^Revision: ' | sed 's/Revision: /r/' | xsel -ib #copyrevision
