7fb71f1800e47bfc53f2cb6cf0773e66d4645c98
scheakur <scheakur@gmail.com>
2015-07-09 23:07:37 +0900

    Refactoring

--- rmbak.sh
+++ rmbak.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-target_list=(vimrc vim gvimrc tmux.conf tmux vrapperrc gitconfig gitignore profile zshrc zshenv config/peco npmrc)
+target_list=(
+    vimrc
+    vim
+    gvimrc
+    tmux.conf
+    tmux
+    vrapperrc
+    gitconfig
+    gitignore
+    profile
+    zshrc
+    zshenv
+    config/peco
+    npmrc
+)
+
 for (( i = 0; i < ${#target_list[@]}; i++ ))
 do
     target=${target_list[i]}
