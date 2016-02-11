883e2ed6f576f22425c933ebe4ab6860bb4402f5
scheakur <scheakur@gmail.com>
2016-02-11 21:50:53 +0900

    Add templates for JavaScript

---
+++ vim/template/javascript/base-jsx.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Xxx extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+Xxx.propTypes = {
+  yyy: PropTypes.string,
+};
