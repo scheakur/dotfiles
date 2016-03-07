42687015a3479872214d9845dee58aa52ef6b1d2
scheakur <scheakur@gmail.com>
2016-03-07 20:51:04 +0900

    Do not use *

--- vim/template/sh/snip-array.sh
+++ vim/template/sh/snip-array.sh
@@ -10,15 +10,8 @@ echo ${array[1]}
 length=${#array[@]}
 echo $length
 
-length=${#array[*]}
-echo $length
-
 for elem in ${array[@]}
 do
     echo $elem
 done
 
-for elem in ${array[*]}
-do
-    echo $elem
-done
