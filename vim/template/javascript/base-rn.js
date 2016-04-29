cba733db6a75336b10d85df1df7a8fae3d5cb9eb
scheakur <scheakur@gmail.com>
2016-04-29 20:56:39 +0900

    Add template for react-native component

---
+++ vim/template/javascript/base-rn.js
@@ -0,0 +1,31 @@
+import React, {
+  Component,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+export default class Xxx extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>xxx</Text>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: '#f00',
+  },
+});
