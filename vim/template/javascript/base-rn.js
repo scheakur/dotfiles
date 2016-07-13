b575ef92a583b419ec832e315469c8829d9b2631
scheakur <scheakur@gmail.com>
2016-07-13 11:06:01 +0900

    Update template for react-native

--- vim/template/javascript/base-rn.js
+++ vim/template/javascript/base-rn.js
@@ -9,28 +9,6 @@ import {
   View,
 } from 'react-native';
 
-export default class Xxx extends Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>xxx</Text>
-      </View>
-    );
-  }
-
-}
-
-
-Xxx.propTypes = {
-  yyy: PropTypes.string,
-};
-
-
-Xxx.defaultProps = {
-  yyy: 'foo',
-};
-
 
 const styles = StyleSheet.create({
   container: {
@@ -43,3 +21,33 @@ const styles = StyleSheet.create({
     color: '#f00',
   },
 });
+
+
+export default class Xxx extends Component {
+
+  render() {
+    return (
+      <View style={[styles.container, this.props.style]}>
+        <Text style={styles.text}>xxx</Text>
+        {this.props.children}
+      </View>
+    );
+  }
+
+}
+
+
+Xxx.propTypes = {
+  yyy: PropTypes.string,
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+    PropTypes.array,
+  ]),
+  children: PropTypes.node,
+};
+
+
+Xxx.defaultProps = {
+  yyy: 'foo',
+};
