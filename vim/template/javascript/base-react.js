249d95f72af2078f0ba2be8dbd218fffd9d2c9ea
scheakur <scheakur@gmail.com>
2016-08-18 13:45:39 +0900

    Update template

--- vim/template/javascript/base-react.js
+++ vim/template/javascript/base-react.js
@@ -2,8 +2,8 @@ import React, { Component, PropTypes } from 'react';
 
 export default class Xxx extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   render() {
