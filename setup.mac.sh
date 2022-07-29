ac7d2b49003262976bf2762b84df01be9645f209
scheakur <scheakur@gmail.com>
2022-07-29 17:40:57 +0900

    Remove unnecessary settings

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,34 +1,6 @@
 #!/bin/sh
 
-target_list=(
-    Applications
-    Desktop
-    Documents
-    Downloads
-    Movies
-    Music
-    Pictures
-    Private
-    Public
-    Work
-    tmp
-)
-
-for (( i = 0; i < ${#target_list[@]}; i++ ))
-do
-    target=${target_list[i]}
-    sudo rm -rf ~/$target
-    ln -si ~/Dropbox/$target ~/$target
-done
-
-killall Finder
-
-
 defaults write com.apple.dock autohide-delay -float 0 
 defaults write com.apple.dock autohide-time-modifier -float 0.24
 
 killall Dock
-
-# vscode
-rm ~/Library/Application\ Support/Code/User/settings.json
-ln -s ~/Dropbox/Work/src/github.com/scheakur/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
