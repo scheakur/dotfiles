e669936ede6aafb42a552f7e363e4365967b455a
scheakur <scheakur@gmail.com>
2023-08-23 16:39:13 +0900

    Add a setting to display app switcher on all monitors

--- setup.mac.sh
+++ setup.mac.sh
@@ -2,5 +2,6 @@
 
 defaults write com.apple.dock autohide-delay -float 0 
 defaults write com.apple.dock autohide-time-modifier -float 0.24
+defaults write com.apple.dock appswitcher-all-displays -bool true
 
 killall Dock
